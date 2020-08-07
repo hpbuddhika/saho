9cef854848de6a5b9f5fbba94b4a8079971173d8
Buddhika Hettiarachchi <hpradeepbuddhika@gmail.com>
2020-08-07 20:25:24 +0530

    added new features

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -6,30 +6,40 @@ var overLayBtnLine2 = document.getElementsByClassName("toggle-btn-line2");
 var overLayBtnLine3 = document.getElementsByClassName("toggle-btn-line3");
 
 function myFunction() {
+  bodyElement[0].style.overflow = "hidden";
+  if (overlay[0].classList.contains("over-lay-add")) {
+    overLayBtnLine2[0].style.opacity = "1";
+    overLayBtnLine1[0].classList.replace(
+      "add-burger-animation",
+      "remove-burger-animation"
+    );
+    overLayBtnLine3[0].classList.replace(
+      "add-burger-animation-line3",
+      "remove-burger-animation-line3"
+    );
 
+    overlay[0].classList.replace("over-lay-add", "over-lay-remove");
+    bodyElement[0].style.overflow = "scroll";
+    // overlay[0].classList.remove("over-lay-add")
+  } else if (overlay[0].classList.contains("over-lay-remove")) {
+    overLayBtnLine2[0].style.opacity = "0";
+    overLayBtnLine1[0].classList.replace(
+      "remove-burger-animation",
+      "add-burger-animation"
+    );
+    overLayBtnLine3[0].classList.replace(
+      "remove-burger-animation-line3",
+      "add-burger-animation-line3"
+    );
 
-  
+    overlay[0].classList.replace("over-lay-remove", "over-lay-add");
+    console.log("class removed");
+  } else {
+    overLayBtnLine2[0].style.opacity = "0";
+    overLayBtnLine1[0].classList.add("add-burger-animation");
+    overLayBtnLine3[0].classList.add("add-burger-animation-line3");
+    console.log(overLayBtnLine1[0]);
 
-
-    bodyElement[0].style.overflow ="hidden"
-    if(overlay[0].classList.contains("over-lay-add")){
-        overLayBtnLine2[0].style.opacity = "1";
-
-        overlay[0].classList.replace("over-lay-add","over-lay-remove")
-        bodyElement[0].style.overflow ="scroll"
-        // overlay[0].classList.remove("over-lay-add")
-    } else if(overlay[0].classList.contains("over-lay-remove")) {
-        overLayBtnLine2[0].style.opacity = "0";
-
-        overlay[0].classList.replace("over-lay-remove","over-lay-add")
-        console.log("class removed")
-    } 
-    else{
-        overLayBtnLine2[0].style.opacity = "0";
-        overLayBtnLine1[0].style.transform= "[rotate(45deg),transform-origin: 20% 40%;]";
-
-        overlay[0].classList.add("over-lay-add")
-
-    }
-    
+    overlay[0].classList.add("over-lay-add");
+  }
 }
